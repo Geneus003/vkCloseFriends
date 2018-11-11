eeddffc35f660dcd208f11f3db52bbf5afcda3e5
geneus003 <geneus003@gmail.com>
2018-11-11 15:08:54 +0300

    All works with just one name

--- get_for_all_friends.py
+++ get_for_all_friends.py
@@ -3,7 +3,7 @@ import get_friend_list
 import check_name_in_file
 
 
-def write_info(first_name, us_id, con, username, passwordik):
+def write_info(first_name, oficial_name, us_id, con, username, passwordik):
     friends_list = get_friend_list.get_friends(us_id, username, passwordik)
 
     if friends_list == [1]:
@@ -21,7 +21,7 @@ def write_info(first_name, us_id, con, username, passwordik):
             if con == 0:
                 print("HERE", i)
             print(i)
-            if friends_list[i]["first_name"] == first_name:
+            if friends_list[i]["first_name"] == first_name or friends_list[i]["first_name"] == oficial_name:
                 friend_info = friends_list[i]["first_name"] + " " + friends_list[i]["last_name"]
                 friend_info += " " + str(friends_list[i]["id"])
                 if check_name_in_file.check_name(friend_info) == 0:
@@ -30,12 +30,12 @@ def write_info(first_name, us_id, con, username, passwordik):
                     f.close()
 
             print(friends_list[i]["first_name"])
-            write_info(first_name, str(friends_list[i]['id']), con+1, username, passwordik)
+            write_info(first_name, oficial_name, str(friends_list[i]['id']), con+1, username, passwordik)
 
     elif con == 2:
         print(friends_list)
         for i in range(len(friends_list)):
-            if friends_list[i]["first_name"] == first_name:
+            if friends_list[i]["first_name"] == first_name or friends_list[i]["first_name"] == oficial_name:
                 friend_info = friends_list[i]["first_name"] + " " + friends_list[i]["last_name"]
                 friend_info += " " + str(friends_list[i]["id"])
                 if check_name_in_file.check_name(friend_info) == 0:
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import get_for_all_friends
 
 
 def main():
-    get_for_all_friends.write_info(1, 1, 1, 1, 1)
+    get_for_all_friends.write_info(1, 1, 1, 1, 1, 1)
 
 
 main()
