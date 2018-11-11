a0aec96751d0032d712d79911abed8e786f5f5e8
geneus003 <geneus003@gmail.com>
2018-11-11 14:36:50 +0300

    Works fine

--- get_for_all_friends.py
+++ get_for_all_friends.py
@@ -6,25 +6,32 @@ def write_info(first_name, us_id, con, username, passwordik):
     friends_list = get_friend_list.get_friends(us_id, username, passwordik)
 
     if friends_list == [1]:
-        return
+        return 0
 
     friends_list = friends_list["items"]
 
-    print(friends_list)
+    if len(friends_list) == 0:
+        return 0
 
-    if con != 1:
-        f = open("us_id", 'tw', encoding='utf-8')
+    print(len(friends_list), con)
+
+    if con < 2:
+        for i in range(len(friends_list)):
+            if con == 0:
+                print("HERE", i)
+            print(i)
+            if friends_list[i]["first_name"] == first_name:
+                f = open("us_id", 'ta', encoding='utf-8')
+                print(friends_list[i]["first_name"], friends_list[i]["last_name"], friends_list[i]["id"], file=f)
+                f.close()
+
+            print(friends_list[i]["first_name"])
+            write_info(first_name, str(friends_list[i]['id']), con+1, username, passwordik)
+
+    elif con == 2:
+        print(friends_list)
         for i in range(len(friends_list)):
             if friends_list[i]["first_name"] == first_name:
-                print(friends_list[i]["first_name"], friends_list[i]["last_name"], friends_list[i]["id"], file=f)
-
-            write_info(first_name, friends_list[i]['id'], con+1, username, passwordik)
-
-        f.close()
-    else:
-        f = open("us_id", 'tw', encoding='utf-8')
-        for i in range(len(friends_list)):
-            if friends_list[i]["first_name"] == first_name:
-                print(friends_list[i]["first_name"], friends_list[i]["last_name"], friends_list[i]["id"], file=f)
-
-            write_info(first_name, friends_list[i]['id'], con, username, passwordik)
+                f = open("us_id", 'ta', encoding='utf-8')
+                print(friends_list[i]["first_name"], friends_list[i]["last_name"], str(friends_list[i]["id"]), file=f)
+                f.close()
